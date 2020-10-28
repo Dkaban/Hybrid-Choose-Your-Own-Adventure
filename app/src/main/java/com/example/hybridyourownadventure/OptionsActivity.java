bc156752be001c92fc644ad34ad15836324ddc46
Dustin Kaban <dustinkaban@gmail.com>
2020-10-27 21:52:19 -0700

    Setup OptionsActivity and LeaderBoardActivity layouts with color coding

--- app/src/main/java/com/example/hybridyourownadventure/OptionsActivity.java
+++ app/src/main/java/com/example/hybridyourownadventure/OptionsActivity.java
@@ -2,12 +2,24 @@ package com.example.hybridyourownadventure;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class OptionsActivity extends AppCompatActivity {
-
+public class OptionsActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+    }
+
+    public void onClickMuteAudio(View view)
+    {
+        //TODO: Mute all Audio
+    }
+
+    public void onClickClearSavedData(View view)
+    {
+        //TODO: Clear all the SharedPreference data
     }
 }
